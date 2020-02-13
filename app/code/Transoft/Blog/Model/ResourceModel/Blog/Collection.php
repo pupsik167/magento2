967be9207181f21aa172f6dee74447af7eff0cc8
pupsik167 <frozetka@gmail.com>
2020-02-13 15:08:44 +0200

    study-63641:Blog module removed redundant code

--- app/code/Transoft/Blog/Model/ResourceModel/Blog/Collection.php
+++ app/code/Transoft/Blog/Model/ResourceModel/Blog/Collection.php
@@ -19,42 +19,4 @@ class Collection extends AbstractCollection
     {
         $this->_init('Transoft\Blog\Model\Blog', 'Transoft\Blog\Model\ResourceModel\Blog');
     }
-
-    /**
-     * Add filter by store
-     *
-     * @param int|array|Store $store
-     * @param bool $withAdmin
-     * @return $this
-     */
-    public function addStoreFilter($store, $withAdmin = true)
-    {
-        $this->performAddStoreFilter($store, $withAdmin);
-
-        return $this;
-    }
-
-    /**
-     * Perform adding filter by store
-     *
-     * @param int|array|Store $store
-     * @param bool $withAdmin
-     * @return void
-     */
-    protected function performAddStoreFilter($store, $withAdmin = true)
-    {
-        if ($store instanceof Store) {
-            $store = [$store->getId()];
-        }
-
-        if (!is_array($store)) {
-            $store = [$store];
-        }
-
-        if ($withAdmin) {
-            $store[] = Store::DEFAULT_STORE_ID;
-        }
-
-        $this->addFilter('store', ['in' => $store], 'public');
-    }
 }
